d453e5736ca0ebef596bf39a879a5ffe713f3d09
zmx6999 <2455847459@qq.com>
2019-05-11 22:26:26 +0800

    Initial commit

--- contract/vote/vote.cpp
+++ contract/vote/vote.cpp
@@ -4,200 +4,189 @@ class youvote: eosio::contract {
 private:
     // @abi table poll
     struct poll {
-        uint64_t key;
         uint64_t poll_id;
         std::string poll_name;
-        uint64_t poll_status = 0;
-        std::string option;
-        uint64_t count = 0;
+        uint64_t poll_status;
+        uint64_t winner_id;
 
-        uint64_t primary_key() const { return key; }
+        uint64_t primary_key() const { return poll_id; }
+    };
+    typedef eosio::multi_index<N(poll), poll> t_poll;
+
+    // @abi table pollopt
+    struct pollopt {
+        uint64_t pollopt_id;
+        uint64_t poll_id;
+        std::string option;
+        uint64_t num;
+
+        uint64_t primary_key() const { return pollopt_id; }
         uint64_t by_poll_id() const { return poll_id; }
     };
-    typedef eosio::multi_index<N(poll), poll, eosio::indexed_by<N(poll_id), eosio::const_mem_fun<poll, uint64_t, &poll::by_poll_id>>> t_poll;
+    typedef eosio::multi_index<N(pollopt), pollopt, eosio::indexed_by<N(poll_id), eosio::const_mem_fun<pollopt, uint64_t, &pollopt::by_poll_id>>> t_pollopt;
 
     // @abi table pollvote
     struct pollvote {
-        uint64_t key;
+        uint64_t pollvote_id;
         uint64_t poll_id;
-        std::string poll_name;
+        uint64_t pollopt_id;
         account_name account;
 
-        uint64_t primary_key() const { return key; }
-        uint64_t by_poll_id() const { return poll_id; }
+        uint64_t primary_key() const { return pollvote_id; }
+        uint64_t by_pollopt_id() const { return pollopt_id; }
     };
-    typedef eosio::multi_index<N(pollvote), pollvote, eosio::indexed_by<N(poll_id), eosio::const_mem_fun<pollvote, uint64_t, &pollvote::by_poll_id>>> t_pollvote;
+    typedef eosio::multi_index<N(pollvote), pollvote, eosio::indexed_by<N(pollopt_id), eosio::const_mem_fun<pollvote, uint64_t, &pollvote::by_pollopt_id>>> t_pollvote;
 
     t_poll _poll;
+    t_pollopt _pollopt;
     t_pollvote _pollvote;
 
 public:
-    youvote(account_name self): eosio::contract(self), _poll(self, self), _pollvote(self, self) {}
+    youvote(account_name self): eosio::contract(self), _poll(self, self), _pollopt(self, self), _pollvote(self, self) {}
 
     // @abi action
     void addpoll(std::string poll_name) {
         require_auth(_self);
         _poll.emplace(_self, [&](auto& p) {
-            p.key = _poll.available_primary_key();
             p.poll_id = _poll.available_primary_key();
             p.poll_name = poll_name;
             p.poll_status = 0;
-            p.option = "";
-            p.count = 0;
+            p.winner_id = 0;
         });
     }
 
     // @abi action
-    void rmpoll(std::string poll_name) {
+    void rmpoll(uint64_t poll_id) {
         require_auth(_self);
 
-        std::vector<uint64_t> keyForDeletion;
-        for (auto& item : _poll) {
-            if (item.poll_name == poll_name) {
-                keyForDeletion.push_back(item.key);
-            }
+        auto itr = _poll.find(poll_id);
+        if (itr == _poll.end() || itr->poll_status != 0) return;
+
+        _poll.erase(itr);
+
+        std::vector<uint64_t> polloptKeyListForDeletion;
+        for (auto& item : _pollopt) {
+            if (item.poll_id == poll_id) polloptKeyListForDeletion.push_back(item.pollopt_id);
         }
 
-        for (uint64_t key : keyForDeletion) {
-            auto itr = _poll.find(key);
-            if (itr != _poll.end()) {
-                _poll.erase(itr);
-            }
-        }
-
-        std::vector<uint64_t> keyForDeletionFromVote;
-        for (auto& item : _pollvote) {
-            if (item.poll_name == poll_name) {
-                keyForDeletionFromVote.push_back(item.key);
-            }
-        }
-
-        for (uint64_t key : keyForDeletionFromVote) {
-            auto itr = _pollvote.find(key);
-            if (itr != _pollvote.end()) {
-                _pollvote.erase(itr);
-            }
+        for (uint64_t key : polloptKeyListForDeletion) {
+            rmpollopt(key);
         }
     }
 
     // @abi action
-    void addpollopt(std::string poll_name, std::string option) {
+    void status(uint64_t poll_id) {
         require_auth(_self);
 
-        for (auto& item : _poll) {
-            if (item.poll_name == poll_name) {
-                if (item.poll_status == 0) {
-                    _poll.emplace(_self, [&](auto& p) {
-                        p.key = _poll.available_primary_key();
-                        p.poll_id = item.poll_id;
-                        p.poll_name = poll_name;
-                        p.poll_status = 0;
-                        p.option = option;
-                        p.count = 0;
-                    });
+        auto itr = _poll.find(poll_id);
+        if (itr == _poll.end()) return;
+
+        if (itr->poll_status >= 2) return;
+
+        if (itr->poll_status == 1) {
+            uint64_t winner_id = 0;
+            uint64_t max = 0;
+            for (auto& item : _pollopt) {
+                if (item.poll_id == poll_id && item.num > max) {
+                    winner_id = item.pollopt_id;
+                    max = item.num;
                 }
-                break;
             }
+
+            _poll.modify(itr, _self, [&](auto& p) {
+                p.poll_status++;
+                p.winner_id = winner_id;
+            });
+
+            return;
         }
+
+        _poll.modify(itr, _self, [&](auto& p) {
+            p.poll_status++;
+        });
     }
 
     // @abi action
-    void rmpollopt(std::string poll_name, std::string option) {
+    void reset(uint64_t poll_id) {
         require_auth(_self);
 
-        std::vector<uint64_t> keyForDeletion;
-        for (auto& item : _poll) {
-            if (item.poll_name == poll_name && item.option == option) {
-                keyForDeletion.push_back(item.key);
-            }
+        auto itr = _poll.find(poll_id);
+        if (itr == _poll.end()) return;
+
+        _poll.modify(itr, _self, [&](auto& p) {
+            p.poll_status = 0;
+        });
+    }
+
+    // @abi action
+    void addpollopt(uint64_t poll_id, std::string option) {
+        require_auth(_self);
+
+        auto itr = _poll.find(poll_id);
+        if (itr == _poll.end()) return;
+
+        if (itr->poll_status != 0) return;
+
+        _pollopt.emplace(_self, [&](auto& p) {
+            p.pollopt_id = _pollopt.available_primary_key();
+            p.poll_id = poll_id;
+            p.option = option;
+            p.num = 0;
+        });
+    }
+
+    // @abi action
+    void rmpollopt(uint64_t pollopt_id) {
+        require_auth(_self);
+
+        auto itr = _pollopt.find(pollopt_id);
+        if (itr == _pollopt.end()) return;
+
+        uint64_t poll_id = itr->poll_id;
+        auto poll_itr = _poll.find(poll_id);
+        if (poll_itr != _poll.end() && poll_itr->poll_status != 0) return;
+
+        _pollopt.erase(itr);
+
+        std::vector<uint64_t> pollvoteKeyListForDeletion;
+        for (auto& item : _pollvote) {
+            if (item.pollopt_id == pollopt_id) pollvoteKeyListForDeletion.push_back(item.pollvote_id);
         }
 
-        for (uint64_t key : keyForDeletion) {
-            auto itr = _poll.find(key);
-            if (itr != _poll.end()) {
-                _poll.erase(itr);
-            }
+        for (uint64_t key : pollvoteKeyListForDeletion) {
+            auto itr = _pollvote.find(key);
+            if (itr != _pollvote.end()) _pollvote.erase(itr);
         }
     }
 
     // @abi action
-    void vote(std::string poll_name, std::string option, account_name account) {
+    void vote(uint64_t pollopt_id, account_name account) {
         require_auth(account);
 
-        std::vector<uint64_t> keyForModify;
-        for (auto& item : _poll) {
-            if (item.poll_name == poll_name && item.option == option) {
-                if (item.poll_status != 1) return;
-                keyForModify.push_back(item.key);
-                break;
-            }
+        auto itr = _pollopt.find(pollopt_id);
+        if (itr == _pollopt.end()) return;
+
+        uint64_t poll_id = itr->poll_id;
+        auto poll_itr = _poll.find(poll_id);
+        if (poll_itr == _poll.end()) return;
+
+        if (poll_itr->poll_status != 1) return;
+
+        for (auto& item : _pollvote) {
+            if (item.poll_id == poll_id && item.account == account) return;
         }
 
-        for (auto& v : _pollvote) {
-            if (v.account == account) return;
-        }
-
-        uint64_t poll_id;
-
-        for (uint64_t key : keyForModify) {
-            auto itr = _poll.find(key);
-            if (itr != _poll.end()) {
-                _poll.modify(itr, _self, [&](auto& p) {
-                    p.count++;
-                });
-                poll_id = itr->poll_id;
-            }
-        }
-
-        _pollvote.emplace(_self, [&](auto& pv) {
-            pv.key = _pollvote.available_primary_key();
-            pv.poll_id = poll_id;
-            pv.poll_name = poll_name;
-            pv.account = account;
+        _pollvote.emplace(_self, [&](auto& p) {
+            p.pollvote_id = _pollvote.available_primary_key();
+            p.poll_id = poll_id;
+            p.pollopt_id = pollopt_id;
+            p.account = account;
         });
-    }
 
-    // @abi action
-    void status(std::string poll_name) {
-        require_auth(_self);
-
-        std::vector<uint64_t> keyForModify;
-        for (auto& item : _poll) {
-            if (item.poll_name == poll_name) {
-                keyForModify.push_back(item.key);
-            }
-        }
-
-        for (uint64_t key : keyForModify) {
-            auto itr = _poll.find(key);
-            if (itr != _poll.end()) {
-                _poll.modify(itr, _self, [&](auto& p) {
-                    p.poll_status++;
-                });
-            }
-        }
-    }
-
-    // @abi action
-    void statusreset(std::string poll_name) {
-        require_auth(_self);
-
-        std::vector<uint64_t> keyForModify;
-        for (auto& item : _poll) {
-            if (item.poll_name == poll_name) {
-                keyForModify.push_back(item.key);
-            }
-        }
-
-        for (uint64_t key : keyForModify) {
-            auto itr = _poll.find(key);
-            if (itr != _poll.end()) {
-                _poll.modify(itr, _self, [&](auto& p) {
-                    p.poll_status = 0;
-                });
-            }
-        }
+        _pollopt.modify(itr, _self, [&](auto& p) {
+            p.num++;
+        });
     }
 };
 
-EOSIO_ABI(youvote, (addpoll)(rmpoll)(addpollopt)(rmpollopt)(vote)(status)(statusreset))
+EOSIO_ABI(youvote, (addpoll)(rmpoll)(status)(reset)(addpollopt)(rmpollopt)(vote))
